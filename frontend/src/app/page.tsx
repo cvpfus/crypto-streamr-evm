4d31b1e1a3a9e4cd014e36f58428747be72d66cd
Yusuf Praditya <47532266+yusufpraditya@users.noreply.github.com>
2025-01-02 08:29:59 +0700

    fix: center the strapline

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div className="flex flex-col gap-4 items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold">Unlock Web3 Monetization with CryptoStreamr</h1>
+      <h1 className="text-3xl text-center mx-4 font-bold">Unlock Web3 Monetization with CryptoStreamr</h1>
 
       <Link href="/dashboard">
         <Button>Launch App</Button>
